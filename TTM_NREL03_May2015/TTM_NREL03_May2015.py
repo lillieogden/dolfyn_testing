6594854df886a8bb370edf30aefad74280d88fbf
lillieogden <lillieogden@comcast.net>
2017-01-25 14:41:56 -0700

    Begin working on creating plots

--- TTM_NREL03_May2015/TTM_NREL03_May2015.py
+++ TTM_NREL03_May2015/TTM_NREL03_May2015.py
@@ -17,12 +17,13 @@ fname = '/Users/lillie/turbulence_data/raw_data/TTM_NREL03_May2015.VEC'
 
 # This is the vector from the ADV head to the body frame, in meters,
 # in the ADV coordinate system.
-body2head_vec = np.array([0.48, -0.07, -0.27])
+body2head_vec = np.array([9.75, 2, -5.75]) * 0.0254
+
 
 # This is the orientation matrix of the ADV head relative to the body.
 # In this case the head was aligned with the body, so it is the
 # identity matrix:
-body2head_rotmat = np.eye(3)
+body2head_rotmat = np.array([[0, 0, -1], [0, -1, 0], [-1, 0, 0]])
 
 # The time range of interest.
 t_range = [
---
+++ TTM_NREL03_May2015/turb_data.py
@@ -0,0 +1,9 @@
+# To get started first import the DOLfYN ADV advanced programming
+# interface (API):
+import dolfyn.adv.api as avm
+
+# Import matplotlib tools for plotting the data:
+from matplotlib import pyplot as plt
+import matplotlib.dates as dt
+import numpy as np
+
