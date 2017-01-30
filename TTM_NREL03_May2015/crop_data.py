4fca880f182f9bc6b8387af962a60e011d1a3b68
lillieogden <lillieogden@comcast.net>
2017-01-30 14:14:18 -0700

    Added code that could determine the t_rangefrom a variance threshold and cleaned up code

--- TTM_NREL03_May2015/crop_data.py
+++ TTM_NREL03_May2015/crop_data.py
@@ -1,43 +1,39 @@
-# To get started first import the DOLfYN ADV advanced programming
-# interface (API):
-import dolfyn.adv.api as avm
+# this script systematically determines the t_ranges indices in which to crop the data
+# by looking at the variance of the u velocity and determing a threshold to compare
+# the values to
 
-# Import matplotlib tools for plotting the data:
-from matplotlib import pyplot as plt
-import matplotlib.dates as dt
+# To get started first import the DOLfYN ADV advanced programming interface (API):
+import dolfyn.adv.turbulence as turb
 import numpy as np
 
-# The file to load:
-fname = '/Users/lillie/turbulence_data/raw_data/TTM_NREL03_May2015.VEC'
 
-# This is the vector from the ADV head to the body frame, in meters,
-# in the ADV coordinate system.
-body2head_vec = np.array([9.75, 2, -5.75]) * 0.0254
+def t_set(advr, n_bin, n_fft=None):
+    # note, can get more accurate t_range by reducing the bin size
 
+    # create a TurbBinner object
+    calculator = turb.TurbBinner(n_bin, advr.fs, n_fft=n_fft)
+    out = turb.VelBinnerSpec.__call__(calculator, advr)
 
-# This is the orientation matrix of the ADV head relative to the body.
-# In this case the head was aligned with the body, so it is the
-# identity matrix:
-body2head_rotmat = np.array([[0, 0, -1], [0, -1, 0], [-1, 0, 0]])
+    # perform the average and variance
+    calculator.do_var(advr, out)
+    calculator.do_avg(advr, out)
 
-# The time range of interest
-# look at a plot of dat.u versus dat.mpltime and can visually see where the data should be cropped
-t_range = [
-    # The instrument was in place starting at 12:08:30 on June 12,
-    # 2012.
-    735729.448,
-    # dt.date2num(dt.datetime.datetime(2012, 6, 12, 12, 8, 30)),
-    # The data is good to the end of the file.
-    735731.305
-]
-# create a function for this that can determine the t_range using variance
+    # add the standard deviation
+    out.add_data('sigma_Uh',
+                 np.std(calculator.reshape(advr.U_mag), -1, dtype=np.float64) -
+                 (advr.noise[0] + advr.noise[1]) / 2, 'main')
 
-# This is the filter to use for motion correction, defined
-accel_filter = 0.1
+    # define just the u velocity variance
+    u_var = out.vel_var[0]
+    # u_vel = out.vel[0]
 
-# End user input section.
-###############################
+    timeset = []
+    for i, j in zip(u_var, out.mpltime):
+        if i < 0:
+            timeset.append(j)
 
-def t_range():
-    # Read a file containing the adv data specified above:
-    dat_raw = avm.read_nortek(fname)
+    start = timeset[0]
+    end = timeset[len(timeset)-1]
+    t_range = [start, end]
+
+    return t_range
--- TTM_NREL03_May2015/velocity_plots.py
+++ TTM_NREL03_May2015/velocity_plots.py
@@ -1,51 +1,15 @@
 # To get started first import the DOLfYN ADV advanced programming
 # interface (API):
 import dolfyn.adv.api as avm
+from crop_data import t_set
 
 # Import matplotlib tools for plotting the data:
 from matplotlib import pyplot as plt
 import matplotlib.dates as dt
 import numpy as np
 
-##############################
-# User input and customization, global variables
 
-# The file to load:
-fname = '/Users/lillie/turbulence_data/raw_data/TTM_NREL03_May2015.VEC'
-
-# This is the vector from the ADV head to the body frame, in meters,
-# in the ADV coordinate system.
-body2head_vec = np.array([9.75, 2, -5.75]) * 0.0254
-
-
-# This is the orientation matrix of the ADV head relative to the body.
-# In this case the head was aligned with the body, so it is the
-# identity matrix:
-body2head_rotmat = np.array([[0, 0, -1], [0, -1, 0], [-1, 0, 0]])
-
-# The time range of interest
-# look at a plot of dat.u versus dat.mpltime and can visually see where the data should be cropped
-t_range = [
-    # The instrument was in place starting at 12:08:30 on June 12,
-    # 2012.
-    735729.450,
-    # dt.date2num(dt.datetime.datetime(2012, 6, 12, 12, 8, 30)),
-    # The data is good to the end of the file.
-    735731.305
-]
-# create a function for this that can determine the t_range using variance
-
-# This is the filter to use for motion correction, defined
-accel_filter = 0.1
-
-# End user input section.
-###############################
-
-
-def crop_clean():
-    # Read a file containing the adv data specified above:
-    dat_raw = avm.read_nortek(fname)
-
+def crop_clean(t_range, dat_raw, body2head_rotmat, body2head_vec):
     # Crop the data for t_range using DOLfYN's 'subset' method (creates a
     # copy):
     t_range_inds = (t_range[0] < dat_raw.mpltime) & (dat_raw.mpltime < t_range[1])
@@ -56,12 +20,33 @@ def crop_clean():
     # Then clean the file using the Goring+Nikora method:
     avm.clean.GN2002(dat)
     dat_cln = dat.copy()
-    return dat_raw, dat, dat_cln, t_range_inds
-
-####
+    return dat, dat_cln, t_range_inds
 
 
-def crop_plot(dat_raw, dat, t_range_inds):
+def motion(dat, dat_cln, accel_filter):
+    """Perform motion correction (including rotation into earth frame):"""
+
+    avm.motion.correct_motion(dat, accel_filter)
+
+    # Rotate the uncorrected data into the earth frame,
+    # for comparison to motion correction:
+    avm.rotate.inst2earth(dat_cln)
+
+    # ax.plot(dat.mpltime, dat.u, 'b-')
+
+    # Then rotate it into a 'principal axes frame':
+    avm.rotate.earth2principal(dat)
+    avm.rotate.earth2principal(dat_cln)
+
+
+# At any point you can save the data:
+# dat_bin.save('adv_data_rotated2principal.h5')
+
+# And reload the data:
+# dat_bin_copy = avm.load('adv_data_rotated2principal.h5')
+
+
+def crop_plot(dat_raw, dat, t_range_inds, t_range):
     """Create a figure for comparing screened data to the original."""
 
     fig = plt.figure(1, figsize=[8, 4])
@@ -87,17 +72,10 @@ def crop_plot(dat_raw, dat, t_range_inds):
             )
 
     # Add some annotations:
-    ax.axvspan(dat_raw.mpltime[0],
-                t_range[0], zorder=-10, facecolor='0.9',
-                edgecolor='none')
-    ax.text(0.13, 1.0, 'Mooring falling\ntoward seafloor',
-            ha='center', va='top', transform=ax.transAxes,
-            size='small')
-    ax.text(0.3, 0.6, 'Mooring on seafloor',
-            ha='center', va='top', transform=ax.transAxes,
-            size='small')
-    ax.annotate('', (0.25, 0.4), (0.4, 0.4),
-                arrowprops=dict(facecolor='black'))
+    ax.axvspan(dat_raw.mpltime[0], t_range[0], zorder=-10, facecolor='0.9', edgecolor='none')
+    ax.text(0.13, 1.0, 'Mooring falling\ntoward seafloor', ha='center', va='top', transform=ax.transAxes, size='small')
+    ax.text(0.3, 0.6, 'Mooring on seafloor', ha='center', va='top', transform=ax.transAxes, size='small')
+    ax.annotate('', (0.25, 0.4), (0.4, 0.4), arrowprops=dict(facecolor='black'))
 
     # Finalize the figure
     # Format the time axis:
@@ -118,39 +96,10 @@ def crop_plot(dat_raw, dat, t_range_inds):
     # Save the figure:
     fig.savefig('/Users/lillie/turbulence_data/plots/crop_data.pdf')
     # end cropping figure
-    ####
-
-
-def motion(dat, dat_cln):
-    '''Perform motion correction (including rotation into earth frame):'''
-
-    avm.motion.correct_motion(dat, accel_filter)
-
-    # Rotate the uncorrected data into the earth frame,
-    # for comparison to motion correction:
-    avm.rotate.inst2earth(dat_cln)
-
-    #ax.plot(dat.mpltime, dat.u, 'b-')
-
-    # Then rotate it into a 'principal axes frame':
-    avm.rotate.earth2principal(dat)
-    avm.rotate.earth2principal(dat_cln)
-
-    # Average the data and compute turbulence statistics
-    dat_bin = avm.calc_turbulence(dat, n_bin=19200,
-                                  n_fft=4096)
-    dat_cln_bin = avm.calc_turbulence(dat_cln, n_bin=19200,
-                                      n_fft=4096)
-    return dat_bin, dat_cln_bin
-
-# At any point you can save the data:
-#dat_bin.save('adv_data_rotated2principal.h5')
-
-# And reload the data:
-# dat_bin_copy = avm.load('adv_data_rotated2principal.h5')
 
 
 def spectra(dat_bin, dat_cln_bin):
+    """Create a plot for looking at the turbulence spectra"""
     # Figure to look at spectra
     fig2 = plt.figure(2, figsize=[6, 6])
     fig2.clf()
@@ -200,10 +149,11 @@ def spectra(dat_bin, dat_cln_bin):
     ax.legend()
     ax.axvspan(1, 16, 0, .2, facecolor='0.8', zorder=-10, edgecolor='none')
     ax.text(4, 4e-4, 'Doppler noise', va='bottom', ha='center',
-            #bbox=dict(facecolor='w', alpha=0.9, edgecolor='none'),
+            # bbox=dict(facecolor='w', alpha=0.9, edgecolor='none'),
             zorder=20)
 
     fig2.savefig('/Users/lillie/turbulence_data/plots/motion_vel_spec.pdf')
+
 
 def turb_energy(dat_cln_bin):
     fig = plt.figure(1, figsize=[8, 4])
@@ -225,7 +175,7 @@ def turb_energy(dat_cln_bin):
     fig.savefig('/Users/lillie/turbulence_data/plots/turb_energy_spec.pdf')
 
 
-def reynolds_Stress(dat_cln_bin):
+def reynolds_stress(dat_cln_bin):
     fig = plt.figure(1, figsize=[8, 4])
     fig.clf()
     ax = fig.add_axes([.14, .14, .8, .74])
@@ -244,11 +194,56 @@ def reynolds_Stress(dat_cln_bin):
 
     fig.savefig('/Users/lillie/turbulence_data/plots/reynolds_stress.pdf')
 
+
 def main():
-    dat_raw, dat , dat_cln, t_range_inds = crop_clean()
-    crop_plot(dat_raw, dat, t_Range_inds)
-    dat_bin, dat_cln_bin = motion(dat, dat_cln)
+    #############################
+    # User input and customization, global variables
+
+    # The file to load:
+    fname = '/Users/lillie/turbulence_data/raw_data/TTM_NREL03_May2015.VEC'
+    # Read a file containing the adv data specified above:
+    dat_raw = avm.read_nortek(fname)
+
+    # This is the vector from the ADV head to the body frame, in meters, in the ADV coordinate system.
+    body2head_vec = np.array([9.75, 2, -5.75]) * 0.0254
+
+    # This is the orientation matrix of the ADV head relative to the body.
+    # In this case the head was aligned with the body, so it is the identity matrix:
+    body2head_rotmat = np.array([[0, 0, -1], [0, -1, 0], [-1, 0, 0]])
+
+    # the time range of interest
+    # 1. look at a plot of dat.u versus dat.mpltime and can visually see where the data should be cropped
+    # t_range = [735729.450, 735731.305]
+
+    # 2. use variance thresholds to determine the t_range
+    t_range = t_set(dat_raw, n_bin=8000, n_fft=4096)
+
+    # This is the filter to use for motion correction, defined
+    accel_filter = 0.1
+
+    # End user input section.
+    ###############################
+
+    # crop and clean the data
+    dat, dat_cln, t_range_inds = crop_clean(t_range, dat_raw, body2head_rotmat, body2head_vec)
+
+    # create a velocity distribution, illustrating the cropping and cleaning process
+    crop_plot(dat_raw, dat, t_range_inds, t_range)
+
+    # perform motion correction
+    motion(dat, dat_cln, accel_filter)
+
+    # Average the data and compute turbulence statistics
+    dat_bin = avm.calc_turbulence(dat, n_bin=19200,
+                                  n_fft=4096)
+    dat_cln_bin = avm.calc_turbulence(dat_cln, n_bin=19200,
+                                      n_fft=4096)
+
+    # make graphs of the velocity spectra, the tke and the reynolds stress
     spectra(dat_bin, dat_cln_bin)
+    turb_energy(dat_cln_bin)
+    reynolds_stress(dat_cln_bin)
 
 
-#run the program
+# run the program
+main()
